c60f7fdf2bc6d7f82d57fe1623901fd3a938b2c0
coder-man-01 <sujal.050619@gmail.com>
2025-07-29 13:07:43 +0530

    add

--- practice/Student.cpp
+++ practice/Student.cpp
@@ -1,25 +1,35 @@
 #include <iostream>
 #include <cstdlib>
-#include <string>
+#include <cstring>
 using namespace std;
- struct Student
- {
- char name[50];
- int age;
- float gpa;
- };
- int main(){
-    int n;
-    cout<<"enter number of students";
-    cin>>n;
-    Student*students=(Student*)malloc(n*sizeof(Student));
-    if (students==nullptr){
-        cout<<"memory allocation failed"<<endl;
-     return 1;
-    }
-    
-        /* code */
+struct Student{
+char name[50];
+int age;
+float gpa;
+};
+int main(){
+int n;
+cout<<"Enter number of students:";
+cin>>n;
+Student* students=(Student*)malloc(n*sizeof(Student));
+
+if (students==nullptr){
+    cout<<"Memory Allocation Failed!"<<endl;
+    return 1;
+}
+for(int i =0;i<n;i++){
+    cout<<"Enter name of Student "<<i+1<<":";
+    cin.ignore();
+    cin.getline(students[i].name,50);
+    cout<<"enter Age:";
+    cin>>students[i].age;
+    cout<<"Enter GPA:";
+    cin>>students[i].gpa;
+}
+cout<<"\n---Student List----\n";
+for(int i=0;i<n;i++){
+    cout<<students[i].name<<"  Age:"<<students[i].age<<"  GPA:"<<students[i].gpa<<endl;
+}
+free(students);
 return 0;
-    
- }
- 
+}
